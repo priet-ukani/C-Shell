ea718fccbc846627ecc8edc25d09f2cbd7db35a7
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 21:00:10 +0530

    Auto-commit changes

--- src/main.c
+++ src/main.c
@@ -60,7 +60,7 @@ int main()
         
         for (int i = 0; i < 15; i++)
         {
-            past_events[i]=substr()
+            past_events[i]=substr(past_events[i], )
             if(strcmp(input,past_events[i])==0)
             {
                 already_exists=!already_exists;
