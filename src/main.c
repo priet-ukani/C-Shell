1923bca23f44f764361e2683ef5bb2309d30a9fc
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 21:15:59 +0530

    Auto-commit changes

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ char *input;
 
 void write_to_file()
 {
-        fopen(file_open_path, "w");
+        File* f = fopen(file_open_path, "w");
         // fopen("/home/prietukani/Desktop/Codes/Coding/Sem 3/OSN/C Shell/C-Shell/src/pastevents.txt", "w");
         
         bool already_exists=false;
