268bc21c33a7dd473ab3c003ff2b3a7566b35fd3
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 17:34:48 +0530

    Auto-commit changes

--- src/peek.h
+++ src/peek.h
@@ -4,15 +4,10 @@
 #include "colours.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/utsname.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define COLOR_DIRECTORY BLUE  
 #define COLOR_EXECUTABLE GREEN 
