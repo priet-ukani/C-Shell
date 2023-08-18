e34df8ce49eac70c4e93ab95177346375e661ac6
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 17:59:16 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -36,9 +36,31 @@ void peek_peek(char*command)
     peek_to=substr(peek_to,ignore_start,strlen(peek_to)-ignore_start);
 
     printf("%d<- len, path-> %s\n", strlen(peek_to), peek_to);
-    char *dir_path 
+    char *dir_path ;
     // = (strlen(peek_to) > 0) ? peek_to : ".";
-    
+        if (strlen(peek_to) > 0) {
+        // If peek_to is a relative path, prepend the current working directory
+        if (peek_to[0] != '/') {
+            char cwd[PATH_MAX];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                dir_path = (char *)malloc(strlen(cwd) + strlen(peek_to) + 2);
+                if (dir_path != NULL) {
+                    sprintf(dir_path, "%s/%s", cwd, peek_to);
+                } else {
+                    perror("Memory allocation error");
+                    return;
+                }
+            } else {
+                perror("Error getting current working directory");
+                return;
+            }
+        } else {
+            dir_path = strdup(peek_to);
+        }
+    } else {
+        dir_path = strdup(".");
+    }
+
     
     DIR *dir = opendir(dir_path);
     if (dir == NULL) {
