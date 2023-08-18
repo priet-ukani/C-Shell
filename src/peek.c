3355e519eaca531979599a18ec2e507dbd28dd5c
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 18:35:12 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -53,72 +53,72 @@ void flags_check(char *peek_to)
 void print_directory_contents(char *path) {
     DIR *dir = opendir(path);
     if (dir) {
-        // Read directory entries and store names in an array
-        struct dirent *entry;
-        size_t entry_count = 0;
-        char **entry_names = NULL;
+    //     // Read directory entries and store names in an array
+    //     struct dirent *entry;
+    //     size_t entry_count = 0;
+    //     char **entry_names = NULL;
 
-        while ((entry = readdir(dir)) != NULL)
-        {
-            entry_names = realloc(entry_names, (entry_count + 1) * sizeof(char *));
-            if (entry_names == NULL)
-            {
-                perror("Memory allocation error");
-                closedir(dir);
-                return;
-            }
-            entry_names[entry_count] = strdup(entry->d_name);
-            if (entry_names[entry_count] == NULL)
-            {
-                perror("Memory allocation error");
-                closedir(dir);
-                return;
-            }
-            entry_count++;
-        }
+    //     while ((entry = readdir(dir)) != NULL)
+    //     {
+    //         entry_names = realloc(entry_names, (entry_count + 1) * sizeof(char *));
+    //         if (entry_names == NULL)
+    //         {
+    //             perror("Memory allocation error");
+    //             closedir(dir);
+    //             return;
+    //         }
+    //         entry_names[entry_count] = strdup(entry->d_name);
+    //         if (entry_names[entry_count] == NULL)
+    //         {
+    //             perror("Memory allocation error");
+    //             closedir(dir);
+    //             return;
+    //         }
+    //         entry_count++;
+    //     }
 
-        // Close the directory
-        closedir(dir);
+    //     // Close the directory
+    //     closedir(dir);
 
-        // Sort the array of entry names
-        qsort(entry_names, entry_count, sizeof(char *), compare_entries);
+    //     // Sort the array of entry names
+    //     qsort(entry_names, entry_count, sizeof(char *), compare_entries);
 
-        // Print sorted entry names
-        for (size_t i = 0; i < entry_count; i++)
-    {
-        char *entry_name = entry_names[i];
-        if (entry_name[0] == '.')
-        {
-            // hidden files to be ignored by default
-            if (!flag_a)
-            {
-                // if not hidden files to be shown then continue;
-                continue;
-            }
-        }
-        struct stat entry_info;
-        if (lstat(entry_name, &entry_info) == -1)
-        {
-            perror("Error getting entry information");
-            continue;
-        }
+    //     // Print sorted entry names
+    //     for (size_t i = 0; i < entry_count; i++)
+    // {
+    //     char *entry_name = entry_names[i];
+    //     if (entry_name[0] == '.')
+    //     {
+    //         // hidden files to be ignored by default
+    //         if (!flag_a)
+    //         {
+    //             // if not hidden files to be shown then continue;
+    //             continue;
+    //         }
+    //     }
+    //     struct stat entry_info;
+    //     if (lstat(entry_name, &entry_info) == -1)
+    //     {
+    //         perror("Error getting entry information");
+    //         continue;
+    //     }
 
-        if (S_ISDIR(entry_info.st_mode))
-        {
-            printf(COLOR_DIRECTORY "%s\n" COLOR_RESET, entry_name);
-        }
-        else if (entry_info.st_mode & S_IXUSR)
-        {
-            printf(COLOR_EXECUTABLE "%s\n" COLOR_RESET, entry_name);
-        }
-        else
-        {
-            printf(COLOR_FILES "%s\n" COLOR_RESET, entry_name);
-        }
+    //     if (S_ISDIR(entry_info.st_mode))
+    //     {
+    //         printf(COLOR_DIRECTORY "%s\n" COLOR_RESET, entry_name);
+    //     }
+    //     else if (entry_info.st_mode & S_IXUSR)
+    //     {
+    //         printf(COLOR_EXECUTABLE "%s\n" COLOR_RESET, entry_name);
+    //     }
+    //     else
+    //     {
+    //         printf(COLOR_FILES "%s\n" COLOR_RESET, entry_name);
+    //     }
 
-        free(entry_names[i]);
-    }
-        free(entry_names);
+    //     free(entry_names[i]);
+    // }
+    //     free(entry_names);
     } else {
         perror("Error opening directory");    
     }
