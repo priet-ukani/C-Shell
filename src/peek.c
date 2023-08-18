3c9f114a14be5ef731724d7b72142bddf9d126f4
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 18:40:47 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -76,6 +76,7 @@ void print_directory_contents(char *path) {
                 }
             }
             struct stat entry_info;
+            
             if (lstat(entry_name, &entry_info) == -1)
             {
                 perror("Error getting entry information");
