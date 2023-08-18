0e659ad24566e8cadf7d392b02e78263e505c993
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 21:19:37 +0530

    Auto-commit changes

--- src/pastevents.h
+++ src/pastevents.h
@@ -1,6 +1,20 @@
 #ifndef PASTEVENTS_H
 #define PASTEVENTS_H
 
+#include <stdio.h>
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "colours.h"
+#include "extra_functions.h"
+
 
 
 #endif
