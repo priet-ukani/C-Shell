00a887b2feeb2264689ed6b6aa32d8e582936c26
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 18:22:15 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -2,60 +2,91 @@
 #include "extra_functions.h"
 #include "peek.h"
 
-int compare_entries(const void *a, const void *b) {
+int compare_entries(const void *a, const void *b)
+{
     return strcmp(*(const char **)a, *(const char **)b);
 }
 
-bool flag_a=false;
-bool flag_l=false;
-int ignore_start=0;
-void flags_check(char* peek_to)
+bool flag_a = false;
+bool flag_l = false;
+int ignore_start = 0;
+void flags_check(char *peek_to)
 {
-    char* temp2=substr(peek_to,0,2);
-    char* temp2_2=substr(peek_to,3,2);
-    char* temp3=substr(peek_to,0,3);
-    if(strcmp(temp3, "-la")==0){ flag_l=flag_a=true;  ignore_start+=3; return;}
-    else if(strcmp(temp3, "-al")==0){ flag_l=flag_a=true;  ignore_start+=3; return;}
-    if(strcmp(temp2, "-l")==0){ flag_l=true; ignore_start+=2;}
-    else if(strcmp(temp2, "-a")==0){ flag_a=true; ignore_start+=2;}
-    if(strcmp(temp2_2, "-l")==0){ flag_l=true;  ignore_start+=3;}
-    else if(strcmp(temp2_2, "-a")==0){ flag_a=true;  ignore_start+=3;}
+    char *temp2 = substr(peek_to, 0, 2);
+    char *temp2_2 = substr(peek_to, 3, 2);
+    char *temp3 = substr(peek_to, 0, 3);
+    if (strcmp(temp3, "-la") == 0)
+    {
+        flag_l = flag_a = true;
+        ignore_start += 3;
+        return;
+    }
+    else if (strcmp(temp3, "-al") == 0)
+    {
+        flag_l = flag_a = true;
+        ignore_start += 3;
+        return;
+    }
+    if (strcmp(temp2, "-l") == 0)
+    {
+        flag_l = true;
+        ignore_start += 2;
+    }
+    else if (strcmp(temp2, "-a") == 0)
+    {
+        flag_a = true;
+        ignore_start += 2;
+    }
+    if (strcmp(temp2_2, "-l") == 0)
+    {
+        flag_l = true;
+        ignore_start += 3;
+    }
+    else if (strcmp(temp2_2, "-a") == 0)
+    {
+        flag_a = true;
+        ignore_start += 3;
+    }
 }
 
-void peek_peek(char*command)
+void peek_peek(char *command)
 {
-    // peek works as ls command to list all files and folders 
-    char* peek_to=substr(command,5,strlen(command)-5);
+    // peek works as ls command to list all files and folders
+    char *peek_to = substr(command, 5, strlen(command) - 5);
 
-    flag_a=false;
-    flag_l=false;
-    ignore_start=0;
+    flag_a = false;
+    flag_l = false;
+    ignore_start = 0;
     flags_check(peek_to);
     // printf("Flag_A: %d, Flag_L: %d\n", flag_a,flag_l);
-    // remove the first ignore_start characters from the peek_to path 
-    peek_to=substr(peek_to,ignore_start,strlen(peek_to)-ignore_start);
+    // remove the first ignore_start characters from the peek_to path
+    peek_to = substr(peek_to, ignore_start, strlen(peek_to) - ignore_start);
 
     // printf("%d<- len, path-> %s\n", strlen(peek_to), peek_to);
-    char *dir_path ;
-    char *path ;
+    char *dir_path;
+    char *path;
     // = (strlen(peek_to) > 0) ? peek_to : ".";
-        if (strlen(peek_to) > 0) {
-        // If peek_to is a relative path, prepend the current working directory
-            if (path[0] != '/' && path[0] != '\\') {
-        char current_dir[256];
-        if (getcwd(current_dir, sizeof(current_dir)) != NULL) {
-            strcat(current_dir, "/");
-            strcat(current_dir, path);
-            strcpy(path, current_dir);
+    if (strlen(peek_to) > 0)
+    {
+        if (path[0] != '/' && path[0] != '\\')
+        {
+            char current_dir[256];
+            if (getcwd(current_dir, sizeof(current_dir)) != NULL)
+            {
+                strcat(current_dir, "/");
+                strcat(current_dir, path);
+                strcpy(path, current_dir);
+            }
         }
     }
-    } else {
+    else
+    {
         dir_path = strdup(".");
     }
 
-    
     DIR *dir = opendir(dir_path);
-    if (dir == NULL) {
+    if (dir == NULL)
+    {
         perror("Error opening directory");
         return;
     }
@@ -65,15 +96,18 @@ void peek_peek(char*command)
     size_t entry_count = 0;
     char **entry_names = NULL;
 
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL)
+    {
         entry_names = realloc(entry_names, (entry_count + 1) * sizeof(char *));
-        if (entry_names == NULL) {
+        if (entry_names == NULL)
+        {
             perror("Memory allocation error");
             closedir(dir);
             return;
         }
         entry_names[entry_count] = strdup(entry->d_name);
-        if (entry_names[entry_count] == NULL) {
+        if (entry_names[entry_count] == NULL)
+        {
             perror("Memory allocation error");
             closedir(dir);
             return;
@@ -88,28 +122,35 @@ void peek_peek(char*command)
     qsort(entry_names, entry_count, sizeof(char *), compare_entries);
 
     // Print sorted entry names
-    for (size_t i = 0; i < entry_count; i++) {
+    for (size_t i = 0; i < entry_count; i++)
+    {
         char *entry_name = entry_names[i];
-        if(entry_name[0]=='.')
+        if (entry_name[0] == '.')
         {
-            // hidden files to be ignored by default 
-            if(!flag_a)
+            // hidden files to be ignored by default
+            if (!flag_a)
             {
                 // if not hidden files to be shown then continue;
                 continue;
             }
         }
         struct stat entry_info;
-        if (lstat(entry_name, &entry_info) == -1) {
+        if (lstat(entry_name, &entry_info) == -1)
+        {
             perror("Error getting entry information");
             continue;
         }
 
-        if (S_ISDIR(entry_info.st_mode)) {
+        if (S_ISDIR(entry_info.st_mode))
+        {
             printf(COLOR_DIRECTORY "%s\n" COLOR_RESET, entry_name);
-        } else if (entry_info.st_mode & S_IXUSR) {
+        }
+        else if (entry_info.st_mode & S_IXUSR)
+        {
             printf(COLOR_EXECUTABLE "%s\n" COLOR_RESET, entry_name);
-        } else {
+        }
+        else
+        {
             printf(COLOR_FILES "%s\n" COLOR_RESET, entry_name);
         }
 
