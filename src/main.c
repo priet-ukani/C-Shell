995481a4c15e841777d56a520f5950cae7cebbb3
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 21:04:37 +0530

    Auto-commit changes

--- src/main.c
+++ src/main.c
@@ -58,8 +58,8 @@ int main()
         // fopen("/home/prietukani/Desktop/Codes/Coding/Sem 3/OSN/C Shell/C-Shell/src/pastevents.txt", "w");
         
         bool already_exists=false;
-        if(input[strlen(input)])
-        strcat(input,"\n");
+        if(input[strlen(input)-1]!='\n')
+            strcat(input,"\n");
         for (int i = 0; i < 15; i++)
         {
             if(strcmp(input,past_events[i])==0)
