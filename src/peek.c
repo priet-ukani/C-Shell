420b9ffa0eb53a7052930ad6334625934a9b4d8c
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 18:06:43 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -38,7 +38,7 @@ void peek_peek(char*command)
     printf("%d<- len, path-> %s\n", strlen(peek_to), peek_to);
     char *dir_path ;
     // = (strlen(peek_to) > 0) ? peek_to : ".";
-    if()
+    if(dir_path=="") return;
     
     
     DIR *dir = opendir(dir_path);
