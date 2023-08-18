3a47efa9b034f84dd55d9fed05d2ecf6187f8262
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 20:56:55 +0530

    Auto-commit changes

--- src/main.c
+++ src/main.c
@@ -15,35 +15,63 @@ int main()
 {
     char *parent_directory = getcwd(NULL, 0); 
     // This gets the current working directory
+    
+    char *line = NULL;
+    char **past_events=(char**)malloc(sizeof(char*)*17);
+    for (int i = 0; i < 17; i++)
+    {
+        past_events[i]=(char*)malloc(sizeof(char)*1024);
+    }
+
     while (1)
     {
-        // read all the commands from the pastevents;
-        FILE* fp = fopen("pastevents", "r");
+        // // read all the commands from the pastevents;
+        FILE* fp;
+        char* file_open_path=(char*)malloc(sizeof(char)*1024);
+        strcpy(file_open_path, parent_directory);
+        strcat(file_open_path, "/pastevents.txt");
+        // printf("%s", file_open_path);
+        fp = fopen(file_open_path, "r");
+        // fp = fopen("/home/prietukani/Desktop/Codes/Coding/Sem 3/OSN/C Shell/C-Shell/src/pastevents.txt", "r");
         if (fp == NULL)
         {
             perror("Error");
             exit(0);
         }
-        char *line = NULL;
-        char **past_events=(char**)malloc(sizeof(char*)*15);
-        for (int i = 0; i < 15; i++)
-        {
-            past_events[i]=(char*)malloc(sizeof(char)*1024);
-        }
         // read all 15 commands from pastevents.txt 
         for (int i = 0; i < 15; i++)
         {
             fgets(past_events[i], 1024, fp);
+            // puts(past_events[i]);
         }
         fclose(fp);
-        
         display_user_prompt_function(parent_directory);
 
         char *input=(char*)malloc(4096*sizeof(char));
         fgets(input,4096,stdin);
-        // This takes the input from the user
+        // // This takes the input from the user
         input=remove_extra_spaces_and_tabs(input);
         struct commands*ok=split_commands(input);
+        
+        for (int k = 13; k >= 0; k--)
+        {
+            // past_events[k+1]=past_events[k];
+            strcpy(past_events[k+1],past_events[k]);
+        }
+        // past_events[0]=input;
+        strcpy(past_events[0],input);
+
+        fopen(file_open_path, "w");
+        // fopen("/home/prietukani/Desktop/Codes/Coding/Sem 3/OSN/C Shell/C-Shell/src/pastevents.txt", "w");
+        for (int i = 0; i < 15; i++)
+        {
+            fprintf(fp, "%s", past_events[i]);
+            if(past_events[i][strlen(past_events[i])-1]!='\n')
+            {
+                fprintf(fp, "\n");
+            }
+        }
+        fclose(fp);
         int i=0;
         while(ok[i].command!=NULL)
         {
@@ -52,17 +80,6 @@ int main()
             execute_function(ok[i]);
             i++;
         }
-        
-        for (int i = 14; i >= 0; i++)
-        {
-            past_events[i+1]=past_events[i];
-        }
-        past_events[i]=input;
-        fp=fopen("pastevents", "w");
-        for (int i = 0; i < 15; i++)
-        {
-            fprintf(fp, "%s", past_events[i]);
-        }        
     }
     return 0;
 }
