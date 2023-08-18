9ab1baf39f8503d602255c9d1f8a3914c13450bf
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 21:06:53 +0530

    Auto-commit changes

--- src/main.c
+++ src/main.c
@@ -62,7 +62,7 @@ int main()
             strcat(input,"\n");
         for (int i = 0; i < 15; i++)
         {
-            
+            past_events[i]=remove_extra_spaces_and_tabs()
             if(strcmp(input,past_events[i])==0)
             {
                 already_exists=!already_exists;
