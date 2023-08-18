08793da2a68af935487f14cc717e6a3f42a59185
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 18:50:50 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -95,6 +95,17 @@ void print_directory_contents(char *path) {
     }
 }
 
+void print_colored(const char *text, const char *color) {
+    printf("%s%s%s", color, text, "\x1b[0m"); // Set color and reset after printing
+}
+
+int is_executable(const char *path) {
+    struct stat st;
+    if (stat(path, &st) == 0) {
+        return (st.st_mode & S_IXUSR) != 0;
+    }
+    return 0;
+}
 
 
 void peek_peek(char *command)
