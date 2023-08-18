dd56c98fe373e8293b31a246400291b575391e17
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 21:16:18 +0530

    Auto-commit changes

--- src/main.c
+++ src/main.c
@@ -17,42 +17,42 @@ char *input;
 
 void write_to_file()
 {
-        FILE* f = fopen(file_open_path, "w");
-        // fopen("/home/prietukani/Desktop/Codes/Coding/Sem 3/OSN/C Shell/C-Shell/src/pastevents.txt", "w");
-        
-        bool already_exists=false;
-        if(input[strlen(input)-1]!='\n')
-            strcat(input,"\n");
-        for (int i = 0; i < 15; i++)
+    FILE* f = fopen(file_open_path, "w");
+    // fopen("/home/prietukani/Desktop/Codes/Coding/Sem 3/OSN/C Shell/C-Shell/src/pastevents.txt", "w");
+    
+    bool already_exists=false;
+    if(input[strlen(input)-1]!='\n')
+        strcat(input,"\n");
+    for (int i = 0; i < 15; i++)
+    {
+        input=remove_extra_spaces_and_tabs(input);
+        past_events[i]=remove_extra_spaces_and_tabs(past_events[i]);
+        if(strcmp(input,past_events[i])==0)
         {
-            input=remove_extra_spaces_and_tabs(input);
-            past_events[i]=remove_extra_spaces_and_tabs(past_events[i]);
-            if(strcmp(input,past_events[i])==0)
-            {
-                already_exists=!already_exists;
-                break;
-            }
+            already_exists=!already_exists;
+            break;
         }
-        // printf("%d\n", already_exists);
-        if(!already_exists)
+    }
+    // printf("%d\n", already_exists);
+    if(!already_exists)
+    {
+        for (int k = 13; k >= 0; k--)
         {
-            for (int k = 13; k >= 0; k--)
-            {
-                // past_events[k+1]=past_events[k];
-                strcpy(past_events[k+1],past_events[k]);
-            }
-            // past_events[0]=input;
-            strcpy(past_events[0],input);
+            // past_events[k+1]=past_events[k];
+            strcpy(past_events[k+1],past_events[k]);
         }
-        for (int i = 0; i < 15; i++)
+        // past_events[0]=input;
+        strcpy(past_events[0],input);
+    }
+    for (int i = 0; i < 15; i++)
+    {
+        fprintf(f, "%s", past_events[i]);
+        if(past_events[i][strlen(past_events[i])-1]!='\n')
         {
-            fprintf(f, "%s", past_events[i]);
-            if(past_events[i][strlen(past_events[i])-1]!='\n')
-            {
-                fprintf(f, "\n");
-            }
+            fprintf(f, "\n");
         }
-        fclose(f);
+    }
+    fclose(f);
 }
 
 int main()
