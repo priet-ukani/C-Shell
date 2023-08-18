f8f23e2160c1c07ad95344fee6e910bf2281d1bd
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 17:50:47 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -96,40 +96,4 @@ void peek_peek(char*command)
         free(entry_names[i]);
     }
     free(entry_names);
-   
-   
-    // DIR *directory = opendir(dir_path);
-    // if(directory==NULL)
-    // {
-    //     perror(RED"Error"RESET);
-    // }
-
-    // struct dirent* entries;
-    // struct dirent** entries_store;
-    // entries_store=(struct dirent **)malloc(sizeof(struct dirent*)*1024);
-    // int entry_count=0;
-    // while((entries=readdir(directory))!=NULL)
-    // {
-    //     entries_store[entry_count++]=entries;
-    // }
-    // qsort(entries_store, entry_count, sizeof(struct dirent*), compare);
-    // for (size_t i = 0; i < entry_count; i++) {
-    //     const char *entry_name = entries_store[i];
-    //     struct stat entry_info;
-    //     if (lstat(entry_name, &entry_info) == -1) {
-    //         perror("Error getting entry information");
-    //         continue;
-    //     }
-
-    //     if (S_ISDIR(entry_info.st_mode)) {
-    //         printf(COLOR_DIRECTORY "%s\n" COLOR_RESET, entry_name);
-    //     } else if (entry_info.st_mode & S_IXUSR) {
-    //         printf(COLOR_EXECUTABLE "%s\n" COLOR_RESET, entry_name);
-    //     } else {
-    //         printf("%s\n", entry_name);
-    //     }
-
-    //     free(entries_store[i]);
-    // }
-    // free(entries);
 }
