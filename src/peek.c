2e660eb96c961b41de43546bf2b8163120f91781
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 17:55:26 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -35,7 +35,7 @@ void peek_peek(char*command)
     // remove the first ignore_start characters from the peek_to path 
     peek_to=substr(peek_to,ignore_start,strlen(peek_to)-ignore_start);
 
-    printf("%d\n", strlen(peek_to));
+    printf("%d %s\n", strlen(peek_to));
     char *dir_path = (strlen(peek_to) > 0) ? peek_to : ".";
     
     DIR *dir = opendir(dir_path);
