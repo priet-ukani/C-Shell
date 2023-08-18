8d922a751a8a92817fdc24435938bd28897ed4a7
priet-ukani <priet.ukani@research.iiit.ac.in>
2023-08-18 18:21:30 +0530

    Auto-commit changes

--- src/peek.c
+++ src/peek.c
@@ -15,7 +15,7 @@ void flags_check(char* peek_to)
     char* temp2_2=substr(peek_to,3,2);
     char* temp3=substr(peek_to,0,3);
     if(strcmp(temp3, "-la")==0){ flag_l=flag_a=true;  ignore_start+=3; return;}
-    else if(strcmp(temp3, "-al")==0){ flag_l=flag_a=true;  ignore_start+=3 ;return;}
+    else if(strcmp(temp3, "-al")==0){ flag_l=flag_a=true;  ignore_start+=3; return;}
     if(strcmp(temp2, "-l")==0){ flag_l=true; ignore_start+=2;}
     else if(strcmp(temp2, "-a")==0){ flag_a=true; ignore_start+=2;}
     if(strcmp(temp2_2, "-l")==0){ flag_l=true;  ignore_start+=3;}
